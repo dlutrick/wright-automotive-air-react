f90fc48ea1bdce267781754deeb9a58907cb5bde
David <davidrlutrick@gmail.com>
2022-04-10 13:36:47 -0700

    added navbar directory with Navbar.js and navbar.css.

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <ul>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+    </ul>
+  );
+};
+
+export default Navbar;
