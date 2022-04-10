2fe130f9fe6d75f8a7a0ec1326cb3e8c179acb4a
David <davidrlutrick@gmail.com>
2022-04-10 14:22:16 -0700

    added basic styles to Navbar.css. Positioned navbar and hid hamburger icon.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,12 +7,17 @@ const Navbar = () => {
     <nav className="navbar">
       <img src="#" className="navbar__logo" />
       <ul className="navbar__links">
-        <li className="navbar__link--active">Home</li>
+        <li className="navbar__link navbar__link--active">Home</li>
         <li className="navbar__link">Gallery</li>
         <li className="navbar__link">Lost Visions Performance</li>
         <li className="navbar__link">About</li>
         <li className="navbar__link">Contact</li>
       </ul>
+      <div className="hamburger">
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
     </nav>
   );
 };
